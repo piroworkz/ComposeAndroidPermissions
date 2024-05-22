e2ecbf2afe86e20ddcfed8a7109658d2a6275b32
David <69691740+piroworkz@users.noreply.github.com>
2024-05-22 09:16:05 -0600

    ver 1.0.0

--- permissions/build.gradle.kts
+++ permissions/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    `maven-publish`
-    signing
+    alias(libs.plugins.mavenPublish)
 }
 
 android {
@@ -51,64 +53,42 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["release"])
-            }
-            groupId = "com.piroworkz"
-            artifactId = "compose-android-permissions"
-            version = "1.0.0"
-
-            pom {
-                name = "ComposeAndroidPermissions"
-                description = "A library to request permissions in Jetpack Compose"
-                url = "https://github.com/piroworkz/ComposeAndroidPermissions"
-                licenses {
-                    license {
-                        name = "Apache-2.0"
-                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "piroworkz"
-                        name = "David Luna"
-                        email = ""
-                    }
-                    scm {
-                        connection =
-                            "scm:git:git://github.com/piroworkz/ComposeAndroidPermissions.git"
-                        developerConnection =
-                            "scm:git:ssh://github.com/piroworkz/ComposeAndroidPermissions.git"
-                        url = "https://github.com/piroworkz/ComposeAndroidPermissions"
-                    }
-                }
-            }
-
-            repositories {
-                maven {
-                    name = "sonatype"
-                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                    credentials {
-                        username = System.getenv("GITHUB_USERNAME")
-                        password = System.getenv("GITHUB_TOKEN")
-                    }
-                }
+mavenPublishing {
+    configure(AndroidSingleVariantLibrary("release", true, true))
+    coordinates("com.piroworkz", "compose-android-permissions", "1.0.0")
+    pom {
+        name.set("ComposeAndroidPermissions")
+        description.set("A library to request android permissions in Jetpack Compose")
+        url.set("https://github.com/piroworkz/ComposeAndroidPermissions")
+        licenses {
+            license {
+                name = "Apache-2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
             }
         }
-    }
-}
 
-signing {
-    useGpgCmd()
-    sign(publishing.publications["release"])
+        developers {
+            developer {
+                id = "piroworkz"
+                name = "David Luna"
+                email = "piroworkz@me.com"
+            }
+        }
+        scm {
+            connection =
+                "scm:git:git://github.com/piroworkz/ComposeAndroidPermissions.git"
+            developerConnection =
+                "scm:git:ssh://github.com/piroworkz/ComposeAndroidPermissions.git"
+            url = "https://github.com/piroworkz/ComposeAndroidPermissions"
+        }
+    }
+    signAllPublications()
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, true)
 }
